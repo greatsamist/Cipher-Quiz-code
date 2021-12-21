428abadc302b52811501f57579feb9a7d85d9912
Samuel Ojo <ifegbenga@gmail.com>
2021-12-21 19:07:59 +0100

    commit

--- Cipher.js
+++ Cipher.js
@@ -1,7 +1,19 @@
 "use strict";
 
-// A function that takes a str
+// A function that takes a str and returns encrypt also decrypt
 
 function rot13(str) {
-  for (var i = 0; i < str.length; i++) {}
+  let converter = "";
+  for (var i = 0; i < str.length; i++) {
+    let asciiNum = str[i].charCodeAt();
+    if (asciiNum >= 65 && asciiNum <= 77) {
+      converter += String.fromCharCode(asciiNum + 13);
+    } else if (asciiNum >= 78 && asciiNum <= 90) {
+      converter += String.fromCharCode(asciiNum - 13);
+    }
+  }
+  return converter;
 }
+//A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+console.log(rot13("PASSWORD")); // CNFFJBEQ
+console.log(rot13("CNFFJBEQ")); // PASSWORD
